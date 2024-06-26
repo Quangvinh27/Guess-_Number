ce61cb53c710ab7938aa0264c9809bce3de9c181
Vinh2712 <vinhnqph28348@fpt.edu.vn>
2024-06-26 10:58:12 +0700

    code 26/6

---
+++ index.js
@@ -0,0 +1,22 @@
+let js = 'amazing';
+        if (js === 'amazing') alert('JavaScript is Fun!')
+//lab14.1
+// khai báo
+let country = "Việt Nam";
+let continent = "Châu Á";
+let population = 98.2;
+            
+//in ra console
+console.log("Tên quốc gia:", country);
+console.log("Lục địa:", continent);
+console.log("Dân số (triệu người):", population);
+
+//lab 15.1
+let isIsland = false;
+let language;
+
+console.log("Kiểu dữ liệu của isIsland:", typeof isIsland);
+console.log("Kiểu dữ liệu của population:", typeof population);
+console.log("Kiểu dữ liệu của country:", typeof country);
+console.log("Kiểu dữ liệu của language:", typeof language);
+
